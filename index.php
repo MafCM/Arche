200e3ed16e0854e7446ad7a1c8f33b7db5d37ba8
MafCM <mf_b_j_w_@hotmail.com>
2019-03-20 14:00:39 -0600

    clase8

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta name="description" content="Arche Museo">
+  <meta name="keywords" content="HTML,CSS,JS,ICONOS,Ejercicio,Programación,código">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+
+  <title>Arche</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+  <link rel="stylesheet" href="css/master.css">
+  <link rel="stylesheet" href="css/responsivos.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Acerca de</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Museo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Tienda</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+  </header>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
--- proyecto/index.php
+++ proyecto/index.php
@@ -1,16 +1,51 @@
+<?php require 'app/app.php'; ?>
 <!doctype html>
 <html lang="es">
-<head>
-  <?php require 'app/components/meta_tags.php'; ?>
-  <title>Arche Museo</title>
-</head>
-<body class="pt-5">
-  <?php require 'app/components/navbar.php'; ?>  
-  <?php require 'app/components/acerca.php'; ?>
-  <?php require 'app/components/bauhaus.php'; ?>
-  <?php require 'app/components/museo.php'; ?>
-  <?php require 'app/components/tienda.php'; ?>
-  <?php require 'app/components/footer.php'; ?>
-  <?php require 'app/components/scripts.php'; ?>
-</body>
+  <head>
+    <?php require 'app/components/meta_tags.php'; ?>
+    <title><?=SITE['name']?></title>
+    <meta name="description" content="¿Necesitas un nuevo sitio web?
+    Elaboro experiencias en línea, hermosas y atractivas. Soy Jonathan MirCha, Diseñador Web.">
+  </head>
+  <body class="pt-5">
+    <?php require 'app/components/navbar.php'; ?>
+    <main class="pt-5 text-white gradient-aqua">
+      <div class="container">
+        <div class="row justify-content-center align-items-center">
+          <article class="col-12 col-lg-5 font-120">
+            <h2>¿Necesitas un increíble sitio web?</h2>
+            <p>
+              Un sitio hermoso, moderno, responsivo y personalizado para tu compañia, negocio, marca, servicio o producto.
+            </p>
+            <a href="#servicios" class="btn btn-lg btn-success">
+              <b>Sí, ¡empecemos!</b>
+            </a>
+          </article>
+          <article class="col-12 col-lg-7">
+            <img class="img-fluid" src="https://placeimg.com/640/480/nature" alt="¿Necesitas un asombroso sitio web?">
+          </article>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row justify-content-center align-items-center">
+          <article class="col-12 col-lg-5 order-lg-1 font-120">
+            <h2>Sitios web responsivos</h2>
+            <p>
+              Es importante proporcionar a tus visitantes una experiencia web óptima en dispositivos móviles. El diseño web responsivo significa que tu sitio se adapta instantáneamente al dispositivo que el visitante utiliza. ¡Mi trabajo es hacer, que la visita de tus usuarios sea lo más agradable e intuitiva para ellos!
+            </p>
+          </article>
+          <article class="col-12 col-lg-7">
+            <img class="img-fluid" src="https://placeimg.com/640/480/animals" alt="¿Necesitas un asombroso sitio web?">
+          </article>
+        </div>
+      </div>
+      <div id="servicios" class="container text-dark">
+        <div class="row justify-content-center align-items-center full-screen">
+          <?php get_service_cards( SITE['service_cards'] ); ?>
+        </div>
+      </div>
+    </main>
+    <?php require 'app/components/footer.php'; ?>
+    <?php require 'app/components/scripts.php'; ?>
+  </body>
 </html>
